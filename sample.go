cc621ddfac087a2e57370f0df0db03d16ee2aecd
Alberto Cortés <alberto@sourced.tech>
2015-12-26 19:51:23 +0100

    Standard deviation and ErrEmptyInputSlice

--- sample.go
+++ sample.go
@@ -7,6 +7,7 @@ package sample
 
 import (
 	"errors"
+	"fmt"
 	"math"
 )
 
@@ -27,4 +28,40 @@ func Mean(s []float64) (float64, error) {
 		sum += sp
 	}
 	return sum / float64(len(s)), nil
+}
+
+// StandardDeviation computes the sample-based unbiased estimation of the
+// standard deviation of a population.
+//
+// It receives the population sample as a slice of float64 and (optionally) its
+// mean. The function returns the estimate standard deviation of the population
+// and a nil error.  If the input parameter has zero elements, it returns
+// math.NaN() and ErrEmptyInputSample.
+//
+// If the provided mean is nil, it will be calculated by the function.
+//
+// This implementation uses the Bessel's correction and therefore needs at
+// least a sample population of 2 sample points or more.
+//
+// This implementation is downward biassed as per Jensen's inequality.
+//
+// It is calculated as sqrt(1/(N-1) sum_i_N(x_i-mean(x))).
+func StandardDeviation(s []float64, mean *float64) (float64, error) {
+	if len(s) == 0 {
+		return math.NaN(), ErrEmptyInputSample
+	}
+	if len(s) == 1 {
+		return math.NaN(), fmt.Errorf("Bessel's correction needs at least two sample points")
+	}
+	if mean == nil {
+		mean = new(float64)
+		*mean, _ = Mean(s) // empty slice error already checked
+	}
+	sum := 0.0
+	for _, sp := range s {
+		diff := sp - *mean
+		squared := diff * diff
+		sum += squared
+	}
+	return math.Sqrt(sum / float64(len(s)-1)), nil
 }
--- sample_test.go
+++ sample_test.go
@@ -5,6 +5,15 @@ import (
 	"testing"
 )
 
+const tolerance = 1e-3
+
+func equals(a, b float64) bool {
+	if math.Abs(a-b) < tolerance {
+		return true
+	}
+	return false
+}
+
 func TestMean(t *testing.T) {
 	for i, f := range [...]struct {
 		in  []float64
@@ -43,7 +52,7 @@ func TestMean(t *testing.T) {
 		if err != nil {
 			t.Errorf("%d) in=%v, returned error: %v", i, f.in, err)
 		}
-		if got != f.out {
+		if !equals(got, f.out) {
 			t.Errorf("%d) in=%v, out=%f, got=%f",
 				i, f.in, f.out, got)
 		}
@@ -53,7 +62,62 @@ func TestMean(t *testing.T) {
 	if !math.IsNaN(got) {
 		t.Errorf("empty slice) got %f, NaN was expected", got)
 	}
-	if err == nil {
-		t.Errorf("empty slice) got no error")
+	if err != ErrEmptyInputSample {
+		t.Errorf("empty slice) got no ErrEmptyInputSample")
+	}
+}
+
+func TestStandardDeviation(t *testing.T) {
+	for i, f := range [...]struct {
+		in  []float64
+		out float64
+	}{
+		{
+			in:  []float64{1.0, 1.0},
+			out: 0.0,
+		},
+		{
+			in:  []float64{1.0, 2.0},
+			out: 0.707,
+		},
+		{
+			in:  []float64{1.0, 2, 3, 4, 5, 6},
+			out: 1.870,
+		},
+		{
+			in:  []float64{-2.0, -1, 0, 1, 2, 3},
+			out: 1.870,
+		},
+	} {
+		got, err := StandardDeviation(f.in, nil)
+		if err != nil {
+			t.Errorf("%d) in=%v, returned error: %v", i, f.in, err)
+		}
+		if !equals(got, f.out) {
+			t.Errorf("%d) in=%v, out=%f, got=%f",
+				i, f.in, f.out, got)
+		}
+		// the same tests, but providing the mean
+		mean, err := Mean(f.in)
+		if err != nil {
+			t.Errorf("%d) [with mean] unexpected error calculating the mean: %s", i, err)
+		}
+		got, err = StandardDeviation(f.in, &mean)
+		if err != nil {
+			t.Errorf("%d) [with mean] in=%v, returned error: %v", i, f.in, err)
+		}
+		if !equals(got, f.out) {
+			t.Errorf("%d) [with mean] in=%v, out=%f, got=%f",
+				i, f.in, f.out, got)
+		}
+	}
+
+	// tests for empty sample
+	got, err := StandardDeviation([]float64{}, nil)
+	if !math.IsNaN(got) {
+		t.Errorf("empty slice) got %f, NaN was expected", got)
+	}
+	if err != ErrEmptyInputSample {
+		t.Errorf("empty slice) returned no ErrEmptyInputSample")
 	}
 }
